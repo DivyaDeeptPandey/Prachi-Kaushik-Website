2e1019f201ccafa74229f0b3b87b1a810ce8620c
DivyaDeeptPandey <divyadeeptpandey@gmail.com>
2025-09-12 18:40:43 +0530

    final

--- src/pages/Initiatives/components/GetInvolved.jsx
+++ src/pages/Initiatives/components/GetInvolved.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { motion } from 'framer-motion';
 import { Link } from 'react-router-dom';
-import SectionHeader from './Sectionheader';
+import SectionHeader from './SectionHeader';
 
 const GetInvolved = ({ sectionRef }) => {
   return (
