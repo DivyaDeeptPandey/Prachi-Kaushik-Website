51c246cbacfc681bf911b01883be2ff05348805e
DivyaDeeptPandey <divyadeeptpandey@gmail.com>
2025-09-12 18:43:39 +0530

    final

--- src/pages/Initiatives/components/MenstrualHealth.jsx
+++ src/pages/Initiatives/components/MenstrualHealth.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { motion } from 'framer-motion';
-import SectionHeader from './Sectionheader';
+import SectionHeader from './SectionHeader';
 import MenstrualHealthImage from '../../../assets/initiatives/scroll-pic6.jpg';
 
 const MenstrualHealth = ({ sectionRef }) => {
